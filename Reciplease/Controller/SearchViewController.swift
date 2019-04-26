0bbfb573796d22dc7e519bff8f369c060c4d7389
MrNasch <nicolaspaschal@hotmail.com>
2019-04-26 15:59:22 +0200

    Starting creation of views and SearchViewController

---
+++ Reciplease/Controller/SearchViewController.swift
@@ -0,0 +1,22 @@
+//
+//  SearchViewController.swift
+//  Reciplease
+//
+//  Created by Nasch on 26/04/2019.
+//  Copyright © 2019 Nasch. All rights reserved.
+//
+
+import UIKit
+
+class SearchViewController: UIViewController {
+
+    @IBAction func didTapSearchForRecipes(_ sender: UIButton) {
+        print("ça marche")
+    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    // MARK: - Navigation
+}
