4e8d60158a5931feabbd1c3cd33e3caaf7d24a76
MrNasch <nicolaspaschal@hotmail.com>
2019-04-30 14:11:08 +0200

    added protocol to the table view of searchListController

--- Reciplease/Controller/SearchListViewController.swift
+++ Reciplease/Controller/SearchListViewController.swift
@@ -15,16 +15,16 @@ class SearchListViewController: UIViewController {
 
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+}
+extension SearchViewController: UITableViewDataSource {
+    // number of section
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
     }
-    */
-
+    // number of rows in section
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // table of recipes.count
+        // return RecipesServices.shared.recipes.count
+    }
+    
 }
